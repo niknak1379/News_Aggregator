0c806a4de50ed86a1b2de938d4454c4a506065aa
niknak1379 <nikanostovan@gmail.com>
2023-12-25 22:47:01 -0800

    fixed filter bug

--- aggregator/urls.py
+++ aggregator/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('sort/', views.sort, name='sort')
+    path('sort/', views.sort, name='sort'),
+    path('filter/', views.custom_filter, name='filter')
 ]
--- aggregator/views.py
+++ aggregator/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse
 from .scrapper import scrapper, vox_scrapper
 from .models import Article
 
+
 # Create your views here.
 
 
@@ -43,8 +44,10 @@ def sort(request):
         """
     articles = Article.objects.all()
     count = Article.objects.count()
+
     if request.method == 'POST':
         sortID = request.POST.get('sort', None)
+
         if sortID == 'author':
             articles = sorted(articles, key=lambda x: x.author)
 
@@ -58,3 +61,39 @@ def sort(request):
     return render(request, "home.html", context)
 
 
+def custom_filter(request):
+    """
+       displays the filtered data based off user input
+
+       checks the POST request called from the form in home.html
+       and depending on the input filters the database and returns
+       the rendered filtered articles
+
+       parameter request: the POST network request issued by the user
+       returns: the rendered filtered articles
+    """
+
+    articles = list(Article.objects.all())
+
+    if request.method == 'POST':
+        sortID = request.POST.get('sort', None)
+        filterStr = request.POST.get('filterField', None)
+
+        if filterStr != "":
+            if sortID == 'author':
+                articles = list(filter(lambda x: filterStr.lower() in x.author.lower(), articles))
+
+            elif sortID == 'title':
+                articles = list(filter(lambda x: filterStr.lower() in x.title.lower(), articles))
+
+    context = {
+        'articles': articles,
+        'count': len(articles)
+    }
+
+    Article.objects.all().delete()
+    for article in articles:
+        Art = Article(title=article.title, author=article.author, source=article.source)
+        Art.save()
+
+    return render(request, "home.html", context)
